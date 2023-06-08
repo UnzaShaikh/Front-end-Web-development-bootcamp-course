476f5f8eb153dd609ea0328c094ae101645c5d74
UnzaShaikh <unzasheikh35@gmail.com>
2023-06-08 12:57:21 +0500

    A js file is added

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+
+// variables and values:
+var myName = 'unza shaikh';
+console.log(myName);
+
+var _1my__Name  = "aliza sheikh";
+console.log(typeof(_1my__Name));
+var myNum = 34;
+console.log(typeof(myNum));
+
+
+
+console.log(9 - "5");
